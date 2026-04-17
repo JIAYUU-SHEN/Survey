b70c33150d7c517f214db7159800e13434f72a3a
Ddddoctor-Jiayu <jiayu.shen@monash.edu>
2026-04-17 15:47:38 +0800

    add Real_vedio batch 5366-6361

--- data/make_json.py
+++ data/make_json.py
@@ -3,13 +3,15 @@ import json
 
 FOLDER_PATH = "seygin/videos/rename"
 CHUNK_SIZE = 100
+START_INDEX = 1  # change this when appending a new folder
 
 FILES = sorted(f"./data/seygin/videos/rename/{f}" for f in os.listdir(FOLDER_PATH) if f.endswith(".mp4"))
 
 for i in range(0, len(FILES), CHUNK_SIZE):
     chunk = FILES[i:i + CHUNK_SIZE]
-    chunk_name = f"{i + 1}-{i + len(chunk)}"
+    start = START_INDEX + i
+    end = start + len(chunk) - 1
 
-    output_file = f"paths_{chunk_name}.json"
+    output_file = f"paths_{start}-{end}.json"
     with open(output_file, "w") as f:
         json.dump(chunk, f, indent=2)
